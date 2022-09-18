5384e9e7ce87673a93b1a1251faa4cae593c3446
diozenio <d.dionisiobsl@gmail.com>
2022-09-18 20:25:08 -0300

    Font loading

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      {fontsLoaded ? <Home /> : <Loading />}
   );
 }
 
